21fa8a7d6cfd75ad8b4576e2d7a929f567f251f2
kilwaz <alexdi@gmail.com>
2019-02-18 15:29:24 +0000

    Full cleanup of wedding and non framework base code.

--- src/main/ngapp/src/app/app.module.ts
+++ src/main/ngapp/src/app/app.module.ts
@@ -5,29 +5,13 @@ import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppRoutingModule} from './/app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
-import {TopbarComponent} from './topbar/topbar.component';
-import {FaqComponent} from './faq/faq.component';
-import {HomeComponent} from './home/home.component';
-import {RsvpComponent} from './rsvp/rsvp.component';
-import {VenueComponent} from './venue/venue.component';
-import {AuthComponent} from './auth/auth.component';
-import {AccommodationComponent} from './accommodation/accommodation.component';
 import {AgGridModule} from 'ag-grid-angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatInputModule, MatRadioModule, MatTreeModule} from '@angular/material';
-import {ScheduleComponent} from './schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopbarComponent,
-    FaqComponent,
-    HomeComponent,
-    RsvpComponent,
-    VenueComponent,
-    AuthComponent,
-    AccommodationComponent,
-    ScheduleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
