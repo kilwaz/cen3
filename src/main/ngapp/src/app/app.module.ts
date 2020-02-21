3f430491823fe6fc7b3e000e0ce36154c7df08d8
kilwaz <alexdi@gmail.com>
2020-02-21 12:48:15 +0000

    Clarity - Package updates, angular material to version 9

--- src/main/ngapp/src/app/app.module.ts
+++ src/main/ngapp/src/app/app.module.ts
@@ -7,20 +7,20 @@ import {AppRoutingModule} from './/app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {OverlayContainer} from '@angular/cdk/overlay';
-import {
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCheckboxModule,
-  MatInputModule,
-  MatProgressBarModule,
-  MatRadioModule,
-  MatTreeModule
-} from '@angular/material';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatTreeModule} from '@angular/material/tree';
+
 import {EchoComponent} from './echo/echo.component';
 import {ClarityComponent} from './clarity/clarity.component';
 import {DndDirective} from './dnd.directive';
 import {MatIconModule} from "@angular/material/icon";
-import { RecordComponent } from './record/record.component';
+import {RecordComponent} from './record/record.component';
 import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
@@ -31,23 +31,23 @@ import {MatTableModule} from "@angular/material/table";
     DndDirective,
     RecordComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatInputModule,
-        MatTreeModule,
-        MatButtonToggleModule,
-        MatProgressBarModule,
-        ReactiveFormsModule,
-        MatIconModule,
-        MatTableModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatInputModule,
+    MatTreeModule,
+    MatButtonToggleModule,
+    MatProgressBarModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatTableModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
